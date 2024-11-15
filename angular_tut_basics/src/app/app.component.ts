44f21c13b61f43689d4cc7bb9e7abe9b165bf0dc
Mihir Tailor <mihirtailor1994@gmail.com>
2024-11-14 22:00:35 -0500

    more details angular

--- angular_tut_basics/src/app/app.component.ts
+++ angular_tut_basics/src/app/app.component.ts
@@ -1,25 +1,36 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Person } from './person';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, CommonModule, FormsModule, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Angular Basics';
   paragraph = 'This is a paragraph';
+  isDisabled = true;
 
-  person = {
+  color_red: string = 'red';
+  bg_color_class: string = 'bg_color';
+
+  imageUrl =
+    'https://cdn.pixabay.com/photo/2024/11/02/15/31/cat-9169528_1280.jpg';
+
+  person: Person = {
     id: 1,
     firstName: 'Bob',
     lastName: 'Marley',
   };
 
-  persons = [
+  persons: Person[] = [
     {
       id: 1,
       firstName: 'Bob',
@@ -36,4 +47,8 @@ export class AppComponent {
       lastName: 'Smith',
     },
   ];
+
+  userSaved() {
+    console.log('User saved');
+  }
 }
